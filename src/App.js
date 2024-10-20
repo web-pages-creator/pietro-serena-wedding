af06c1dec3b9ebf09747fb0fb095744068db7177
PeterQuadrini <webpagescreator.corp@gmail.com>
2024-10-20 13:31:19 +0200

    ciao

--- src/App.js
+++ src/App.js
@@ -1,14 +1,17 @@
-import { Route, Routes } from 'react-router-dom';
+import { HashRouter, Route, Routes } from 'react-router-dom';
 import './App.css';
 import RSVPWithParams from './pages/RSVPWithParams';
+import Default from './pages/Default';
 
 function App() {
   return (
     <div className='w-screen h-screen h-auto flex flex-col items-center justify-center'>
-
-      <Routes>
-        <Route path="/rsvp/:id" element={<RSVPWithParams />} />
-      </Routes>
+      <HashRouter>
+        <Routes>
+          <Route path="/rsvp/:id" element={<RSVPWithParams />} />
+          <Route path="*" element={<Default />} />
+        </Routes>
+      </HashRouter>
     </div>
   );
 }
---
+++ src/pages/Default.js
@@ -0,0 +1,12 @@
+
+
+function Default() {
+
+  return (
+    <div className=''>
+      CIao default
+    </div>
+  );
+}
+
+export default Default;
