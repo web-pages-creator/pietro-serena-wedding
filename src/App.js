a4cb23ff84004ceaa91cbadca89851cf67a2c053
PeterQuadrini <webpagescreator.corp@gmail.com>
2024-10-21 17:07:22 +0200

    ok

--- src/App.js
+++ src/App.js
@@ -6,12 +6,10 @@ import Default from './pages/Default';
 function App() {
   return (
     <div className='w-screen h-screen h-auto flex flex-col items-center justify-center'>
-      <HashRouter>
         <Routes>
           <Route path="/rsvp/:id" element={<RSVPWithParams />} />
           <Route path="*" element={<Default />} />
         </Routes>
-      </HashRouter>
     </div>
   );
 }
